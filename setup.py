e7ec8dbce62885409b203f7d8d38188dfd8769dd
ktss1219 <shusakukita@gmail.com>
2023-09-14 16:26:57 +0900

    edit

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='asm2vec',
     version='1.0.0',
     description='Unofficial implementation of asm2vec using pytorch',
-    install_requires=['torch>=1.7,<2'
-                      'click>=7.1,<8'
-                      'r2pipe>=1.5,<2'],
+    install_requires=['torch',
+                      'click',
+                      'r2pipe'],
     author='oalieno',
     author_email='jeffrey6910@gmail.com',
     license='MIT License',
